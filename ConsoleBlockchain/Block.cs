2b1497d8f71915ca602ace14be62a60b2e8c4638
Jesse Harlan <gotsanity@gmail.com>
2022-02-09 12:33:28 -0800

    Add project files.

---
+++ ConsoleBlockchain/Block.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBlockchain
+{
+    public class Block
+    {
+        public int BlockID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public byte[] BlockHash { get; set; }
+        public byte[] PrevHash { get; set; }
+        public Payload Data { get; set; }
+
+        public Block(int blockID, byte[] prevHash, Payload data)
+        {
+            BlockID = blockID;
+            Timestamp = DateTime.Now;
+            PrevHash = prevHash;
+            Data = data;
+
+            this.BlockHash = this.GetHash();
+        }
+
+        public byte[] GetHash()
+        {
+            SHA256 sha256hash = SHA256.Create();
+
+            string rawData = this.BlockID.ToString() +
+                this.Timestamp.ToString() +
+                Block.getHashString(this.PrevHash) +
+                this.Data.ToString();
+
+            byte[] bytes = sha256hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            return bytes;
+        }
+
+        public static string getHashString(byte[] hash)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                builder.Append(hash[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+
+        public override string? ToString()
+        {
+            return this.BlockID.ToString() + "\t" +
+                this.Timestamp.ToString() + "\t" +
+                Block.getHashString(this.BlockHash) + "\t" +
+                Block.getHashString(this.PrevHash) + "\t" +
+                this.Data.ToString();
+        }
+    }
+
+    public class BlockChain
+    {
+        public List<Block> Chain { get; set; }
+
+        public BlockChain()
+        {
+            this.Chain = new List<Block>();
+        }
+
+        public void AddBlock(Payload payload)
+        {
+            int blockID = this.Chain.Count;
+            byte[] prevHash = this.Chain.Count != 0 ? this.Chain[this.Chain.Count - 1].BlockHash : new byte[0];
+
+            Block block = new Block(blockID, prevHash, payload);
+
+            this.Chain.Add(block);
+        }
+
+        public decimal FindBalance(string person)
+        {
+
+            return 0.0m;
+        }
+    }
+
+    public class Payload
+    {
+        public DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Payee { get; set; }
+        public string Payer { get; set; }
+        public string Reason { get; set; }
+
+        public Payload(DateTime transactionDate, decimal amount, string payee, string payer, string reason)
+        {
+            TransactionDate = transactionDate;
+            Amount = amount;
+            Payee = payee;
+            Payer = payer;
+            Reason = reason;
+        }
+
+        public override string? ToString()
+        {
+            return TransactionDate.ToString() + "\t" + Payee + "\t" + Payer + "\t" + Reason + "\t" + Amount;
+        }
+    }
+}
---
+++ ConsoleBlockchain/Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleBlockchain;
+
+Payload payload = new Payload(DateTime.Now, 100.00m, "me", "you", "birthday");
+Payload payload2 = new Payload(DateTime.Now, 100.00m, "you", "them", "games");
+Payload payload3 = new Payload(DateTime.Now, 50.00m, "us", "them", "games");
+Payload payload4 = new Payload(DateTime.Now, 50.00m, "they", "them", "games");
+
+BlockChain chain = new BlockChain();
+
+chain.AddBlock(payload);
+chain.AddBlock(payload2);
+chain.AddBlock(payload3);
+chain.AddBlock(payload3);
+chain.AddBlock(payload4);
+
+for (int i = 0; i < chain.Chain.Count; i++)
+{
+    Console.WriteLine(chain.Chain[i].ToString());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * 
+ * ledger
+ * 
+ * 99 btc pays alice 100
+ * 1 bob pays alice 100
+ * 2 alice pays sam 50 <alice signature>
+ * 3 alice pays sam 50 <alice signature>
+ * sam pay alice 100
+ * 4 btc pays sam 1btc <sig>
+ */
+
+
+// cryptographic hash function
+
+// math function accepts an input and spits out a "randomized" output
+
+/*
+ * f(x) = x + 1
+ * f(1) = 2
+ * f(3) = 4
+ * 
+ * SHA256("TEXT TO fILES") = <insert 2^256 bits has code here>
+ * 
+ * 2^256 = (2^32)(2^32)(2^32)(2^32)(2^32)(2^32)(2^32)(2^32) = (4billion)(4billion)(4billion)(4billion)(4billion)(4billion)(4billion)(4billion)
+ * 
+ * public key: 1234
+ * private key: 5678
+ * 
+ * hash(5678) = 1234
+ * 
+ * 
+ * message("hi mom", 1234) = 000000000000132136541321654321
+ * 
+ */
+
+/*
+ * Blockchain
+ * 
+ * current data + previous hash + index = new hash
+ */
